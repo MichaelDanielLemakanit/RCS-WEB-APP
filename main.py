5eda5b39d622737954a4e8fed464f5be8f19f971
Daniel michael <michaellemakanit@gmail.com>
2026-09-14 23:54:46 +0300

    newrefs

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ def home():
     return render_template('layout.html')
 
 
+@app.route('/school-life')
+def school_life():
+    return render_template('school_life.html')
 
+@app.route('/enrol')
+def enrol():
+    return render_template('enrol.html')
 
+@app.route('/careers')
+def careers():
+    return render_template('careers.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 
